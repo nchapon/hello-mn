8d609d7874b47db5985f6ab363214449f215ff03
nchapon <nchapon@gmail.com>
2020-07-21 17:43:19 +0200

    FaulTolerance : use CircuitBreaker

--- src/main/java/hello/mn/domain/FruityServiceClient.java
+++ src/main/java/hello/mn/domain/FruityServiceClient.java
@@ -1,9 +1,10 @@
 package hello.mn.domain;
 
 import io.micronaut.http.client.annotation.Client;
-import io.micronaut.retry.annotation.Retryable;
+import io.micronaut.retry.annotation.CircuitBreaker;
 
 @Client("fruity")
-@Retryable(attempts = "5", delay = "100ms")
+//@Retryable(attempts = "5", delay = "100ms")
+@CircuitBreaker
 public interface FruityServiceClient extends FruityService {
 }
