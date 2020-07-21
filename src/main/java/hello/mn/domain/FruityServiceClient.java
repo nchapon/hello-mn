4c81914b3e89e752f90077b0074714c01602ca7a
nchapon <nchapon@gmail.com>
2020-07-21 17:58:02 +0200

    FaultTolerance : fix @Retryable with fallback

--- src/main/java/hello/mn/domain/FruityServiceClient.java
+++ src/main/java/hello/mn/domain/FruityServiceClient.java
@@ -1,10 +1,11 @@
 package hello.mn.domain;
 
 import io.micronaut.http.client.annotation.Client;
-import io.micronaut.retry.annotation.CircuitBreaker;
+import io.micronaut.retry.annotation.Recoverable;
+import io.micronaut.retry.annotation.Retryable;
 
 @Client("fruity")
-//@Retryable(attempts = "5", delay = "100ms")
-@CircuitBreaker
+@Retryable(attempts = "5", delay = "100ms")
+@Recoverable(api=FruityService.class)
 public interface FruityServiceClient extends FruityService {
 }
